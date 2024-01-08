35c3c7742057b869e84731278fd7b555230fd7f6
Alois Sečkár <ellrohir@seznam.cz>
2024-01-08 22:06:17 +0100

    refactor - replace \u00A0 with shorter \xa0

--- src/text/textUtils.ts
+++ src/text/textUtils.ts
@@ -1,4 +1,4 @@
-// VERSION: 1.3
+// VERSION: 1.3.1
 // LAST UPDATE: 2024-01-08
 // DESCRIPTION:
 // replace single-letter words from being left alone at the end of the line
@@ -8,18 +8,18 @@ export function preventSingleLetterOrphans (input: string): string {
     if (input) {
       // single-letter words (CS language)
       // lower case - possible combinations
-      input = input.replace(/([,;]?\s)([ai]) ([ikosuvz]) /g, '$1$2\u00A0$3\u00A0')
+      input = input.replace(/([,;]?\s)([ai]) ([ikosuvz]) /g, '$1$2\xa0$3\xa0')
       // lower case
-      input = input.replace(/([,;]?\s)([aikosuvz]) /g, '$1$2\u00A0')
+      input = input.replace(/([,;]?\s)([aikosuvz]) /g, '$1$2\xa0')
       // upper case- possible combinations
-      input = input.replace(/([\.?!:]?\s?)([AI]) ([ikosuvz]) /g, '$1$2\u00A0$3\u00A0')
+      input = input.replace(/([\.?!:]?\s?)([AI]) ([ikosuvz]) /g, '$1$2\xa0$3\xa0')
       // upper case
-      input = input.replace(/([\.?!:]?\s?)([AIKOSUVZ]) /g, '$1$2\u00A0')
+      input = input.replace(/([\.?!:]?\s?)([AIKOSUVZ]) /g, '$1$2\xa0')
       // other special symbols
-      input = input.replace(' - ', ' -\u00A0')
-      input = input.replace(' č. ', ' č.\u00A0')
-      input = input.replace(' %', '\u00A0%')
-      input = input.replace(' Sb.', '\u00A0Sb.')
+      input = input.replace(' - ', ' -\xa0')
+      input = input.replace(' č. ', ' č.\xa0')
+      input = input.replace(' %', '\xa0%')
+      input = input.replace(' Sb.', '\xa0Sb.')
     }
     return input
   }
--- test/text/preventSingleLetterOrphans.test.ts
+++ test/text/preventSingleLetterOrphans.test.ts
@@ -4,47 +4,47 @@ import { preventSingleLetterOrphans } from '../../src/main'
 test('should add non-breaking space after "a"', () => {
   const text = preventSingleLetterOrphans("Ahoj a nashledanou!")
   expect(text).not.toBe("Ahoj a nashledanou!")
-  expect(text).toBe("Ahoj a\u00A0nashledanou!")
+  expect(text).toBe("Ahoj a\xa0nashledanou!")
 })
 
 test('should keep text intact', () => {
   const text = preventSingleLetterOrphans("Ahoj b nashledanou!")
   expect(text).toBe("Ahoj b nashledanou!")
-  expect(text).not.toBe("Ahoj b\u00A0nashledanou!")
+  expect(text).not.toBe("Ahoj b\xa0nashledanou!")
 })
 
 test('should add non-breaking space after all "s"', () => {
   const text = preventSingleLetterOrphans("Pštros s pštrosicí s pštorsáčaty")
-  expect(text).not.toBe("Pštros s\u00A0pštrosicí s pštorsáčaty")
-  expect(text).toBe("Pštros s\u00A0pštrosicí s\u00A0pštorsáčaty")
+  expect(text).not.toBe("Pštros s\xa0pštrosicí s pštorsáčaty")
+  expect(text).toBe("Pštros s\xa0pštrosicí s\xa0pštorsáčaty")
 })
 
 test('should add non-breaking space after capital "K"', () => {
   const text = preventSingleLetterOrphans("Ulice jménem K Novým dvorům")
   expect(text).not.toBe("Ulice jménem K Novým dvorům")
-  expect(text).toBe("Ulice jménem K\u00A0Novým dvorům")
+  expect(text).toBe("Ulice jménem K\xa0Novým dvorům")
 })
 
 test('should add non-breaking space after "a", "s" and "i"', () => {
   const text = preventSingleLetterOrphans("Jan a s ním i Jana")
-  expect(text).not.toBe("Jan a\u00A0s ním i Jana")
-  expect(text).not.toBe("Jan a\u00A0s ním i\u00A0Jana")
-  expect(text).toBe("Jan a\u00A0s\u00A0ním i\u00A0Jana")
+  expect(text).not.toBe("Jan a\xa0s ním i Jana")
+  expect(text).not.toBe("Jan a\xa0s ním i\xa0Jana")
+  expect(text).toBe("Jan a\xa0s\xa0ním i\xa0Jana")
 })
 
 test('should add non-breaking space after capital "A" at the begining', () => {
   const text = preventSingleLetterOrphans("A to je vše")
   expect(text).not.toBe("A to je vše")
-  expect(text).toBe("A\u00A0to je vše")
+  expect(text).toBe("A\xa0to je vše")
 })
 
 test('should add non-breaking space to appropriate places', () => {
   const text1 = preventSingleLetterOrphans("Hle, a to je vše.")
-  expect(text1).toBe("Hle, a\u00A0to je vše.")
+  expect(text1).toBe("Hle, a\xa0to je vše.")
   const text2 = preventSingleLetterOrphans("Hle: a s tím je to vše.")
-  expect(text2).toBe("Hle: a\u00A0s\u00A0tím je to vše.")
+  expect(text2).toBe("Hle: a\xa0s\xa0tím je to vše.")
   const text3 = preventSingleLetterOrphans("Hle. A to je vše.")
-  expect(text3).toBe("Hle. A\u00A0to je vše.")
+  expect(text3).toBe("Hle. A\xa0to je vše.")
   const text4 = preventSingleLetterOrphans("Hle: A s tím je to vše.")
-  expect(text4).toBe("Hle: A\u00A0s\u00A0tím je to vše.")
+  expect(text4).toBe("Hle: A\xa0s\xa0tím je to vše.")
 })
