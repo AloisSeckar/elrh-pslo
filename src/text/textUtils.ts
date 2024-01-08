4ddaf973c9001714f8082b5c8bcadf3fa4e43c31
Alois Sečkár <ellrohir@seznam.cz>
2024-01-08 18:39:36 +0100

    fix: add letter "u"

--- src/text/textUtils.ts
+++ src/text/textUtils.ts
@@ -13,6 +13,7 @@ export function preventSingleLetterOrphans (input: string): string {
       input = input.replaceAll(' k ', ' k\u00A0')
       input = input.replaceAll(' o ', ' o\u00A0')
       input = input.replaceAll(' s ', ' s\u00A0')
+      input = input.replaceAll(' u ', ' u\u00A0')
       input = input.replaceAll(' v ', ' v\u00A0')
       input = input.replaceAll(' z ', ' z\u00A0')
       // upper case
@@ -21,6 +22,7 @@ export function preventSingleLetterOrphans (input: string): string {
       input = input.replaceAll(' K ', ' K\u00A0')
       input = input.replaceAll(' O ', ' O\u00A0')
       input = input.replaceAll(' S ', ' S\u00A0')
+      input = input.replaceAll(' U', '  U\u00A0')
       input = input.replaceAll(' V ', ' V\u00A0')
       input = input.replaceAll(' Z ', ' Z\u00A0')
       // other special symbols
