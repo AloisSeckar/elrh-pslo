e26960bf783fba41dd66e827954e5c0c73dec62b
Alois Sečkár <ellrohir@seznam.cz>
2024-01-13 15:50:49 +0100

    chore: indent

--- src/text/textUtils.ts
+++ src/text/textUtils.ts
@@ -5,21 +5,21 @@
 // this is especially important in Czech language, where it is considered
 // a typography error
 export function preventSingleLetterOrphans (input: string): string {
-    if (input) {
-      // single-letter words (CS language)
-      // lower case - possible combinations
-      input = input.replace(/([,;\)]?\s\(?)([ai])\s([ikosuvz])\s/g, '$1$2\xa0$3\xa0')
-      // lower case
-      input = input.replace(/([,;\)]?\s\(?)([aikosuvz])\s/g, '$1$2\xa0')
-      // upper case- possible combinations
-      input = input.replace(/([\.?!:\)]?\s?\(?)([AI])\s([ikosuvz])\s/g, '$1$2\xa0$3\xa0')
-      // upper case
-      input = input.replace(/([\.?!:\)]?\s?\(?)([AIKOSUVZ])\s/g, '$1$2\xa0')
-      // other special symbols
-      input = input.replace(' - ', ' -\xa0')
-      input = input.replace(' č. ', ' č.\xa0')
-      input = input.replace(' %', '\xa0%')
-      input = input.replace(' Sb.', '\xa0Sb.')
-    }
-    return input
+  if (input) {
+    // single-letter words (CS language)
+    // lower case - possible combinations
+    input = input.replace(/([,;\)]?\s\(?)([ai])\s([ikosuvz])\s/g, '$1$2\xa0$3\xa0')
+    // lower case
+    input = input.replace(/([,;\)]?\s\(?)([aikosuvz])\s/g, '$1$2\xa0')
+    // upper case- possible combinations
+    input = input.replace(/([\.?!:\)]?\s?\(?)([AI])\s([ikosuvz])\s/g, '$1$2\xa0$3\xa0')
+    // upper case
+    input = input.replace(/([\.?!:\)]?\s?\(?)([AIKOSUVZ])\s/g, '$1$2\xa0')
+    // other special symbols
+    input = input.replace(' - ', ' -\xa0')
+    input = input.replace(' č. ', ' č.\xa0')
+    input = input.replace(' %', '\xa0%')
+    input = input.replace(' Sb.', '\xa0Sb.')
   }
+  return input
+}
