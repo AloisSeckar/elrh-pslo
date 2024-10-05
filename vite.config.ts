6e5738c4c910a5b8370d6142e8e2b7958981704a
Alois Sečkár <ellrohir@seznam.cz>
2024-10-05 08:29:16 +0200

    feat: improve configuration as library module

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,17 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
+// https://vite.dev/guide/build.html#library-mode
 export default defineConfig({
-  build: { lib: { entry: resolve(__dirname, 'src/main.ts'), formats: ['es'] } },
-  resolve: { alias: { src: resolve('src/') } },
+  build: { 
+    lib: { 
+      entry: resolve(__dirname, 'src/main.ts'), 
+      formats: ['es', 'cjs'] 
+    } 
+  },
+  resolve: { 
+    alias: { 
+      src: resolve('src/') 
+    } 
+  },
 })
